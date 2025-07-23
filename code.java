023feb93ea8047b36785aea1821593de213d785a
mohammadibrhaim273 <smohammadibrahim118@gmail.com>
2025-07-23 14:41:09 +0530

    iii

--- code.java
+++ code.java
@@ -1,6 +1,9 @@
 public class code {
     public static void main (String []args){
         System.out.println("Hello wordgit ");
+        System.out.println("mohammahad ibrahim");
+        System.out.println("iiiiiii");
+        System.out.println("ffffffff");
     }
     
 }
--- first.java
+++ first.java
@@ -2,6 +2,7 @@ public class first {
     public static void main (String []args){
         System.out.println("My name is ibrahim");
         System.out.println("my age is 20");
+        System.out.println("hello hello hello ");
     }
     
 }
