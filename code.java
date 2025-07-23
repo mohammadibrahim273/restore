d1f055cc8f131efe8a6097f56f7b7dc2a5515d93
mohammadibrhaim273 <smohammadibrahim118@gmail.com>
2025-07-23 15:07:28 +0530

    resert

--- code.java
+++ code.java
@@ -1,9 +1,6 @@
 public class code {
     public static void main (String []args){
         System.out.println("Hello wordgit ");
-        System.out.println("mohammahad ibrahim");
-        System.out.println("iiiiiii");
-        System.out.println("ffffffff");
     }
     
 }
---
+++ reset.java
@@ -0,0 +1,6 @@
+public class reset {
+    public static void main (String [] arg){
+        System.out.println("ibrahim");
+    }
+    
+}
