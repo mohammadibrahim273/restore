417f6965dc089f8c88bf5147394c8f70d42da403
mohammadibrhaim273 <smohammadibrahim118@gmail.com>
2025-07-24 14:58:30 +0530

    try

--- reset.java
+++ reset.java
@@ -2,6 +2,7 @@ public class reset {
     public static void main (String [] arg){
         System.out.println("ibrahim");
         System.out.println("reset use kar na hai");
+        System.out.println("nahi hora");
     }
     
 }
